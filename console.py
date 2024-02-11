727d3a3171606a37285a449478ea809756d3fdc4
AshTechs <ashbelh@gmail.com>
2024-02-11 14:47:31 -0800

    Console

--- console.py
+++ console.py
@@ -1,4 +1,3 @@
-```python
 #!/usr/bin/python3
 """
 Defines the HBnB console.
@@ -25,15 +24,15 @@ def parse(arg):
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
         if brackets is None:
-            return [i.strip(",") for i in split(arg)]
+            return [x.strip(",") for x in split(arg)]
         else:
             lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
+            retl = [x.strip(",") for x in lexer]
             retl.append(brackets.group())
             return retl
     else:
         lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
+        retl = [x.strip(",") for x in lexer]
         retl.append(curly_braces.group())
         return retl
 
@@ -106,7 +105,7 @@ class HBNBCommand(cmd.Cmd):
         if len(argl) == 0:
             print("** class name missing **")
         elif argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
+            print("** Class doesn't exist **")
         else:
             print(eval(argl[0])().id)
             storage.save()
